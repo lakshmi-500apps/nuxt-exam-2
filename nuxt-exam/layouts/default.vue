40d833c312345a471e6a60e83d9d0704b5fccb11
lakshmi-500apps <lakshmi.lagadapati@500apps.com>
2023-04-08 16:21:52 +0530

    Implemented Mockinterviews

--- nuxt-exam/layouts/default.vue
+++ nuxt-exam/layouts/default.vue
@@ -1,10 +1,15 @@
 <template>
    <nav class="container mx-auto p-4 ">
-              <NuxtLink to="/testcollection" class="font-bold">Bank</NuxtLink>
-              <NuxtLink to="/project" class="ml-2 font-bold">Project</NuxtLink>
-          </nav>
-
+              <div class="flex justify-evenly">
+                <div><NuxtLink to="/testcollection" class="font-bold"><div class="flex ml-2"><div><ClipboardDocumentIcon class="h-6 w-6 mr-2"/></div><div>Bank</div></div></NuxtLink></div>
+              <div><NuxtLink to="/project" class="font-bold"><div class="flex ml-2"><div><BuildingOfficeIcon class="h-6 w-6 mr-2"/></div><div>Project</div></div></NuxtLink></div>
+              <div><NuxtLink to="/mockinterview" class="font-bold"><div class="flex ml-2"><div><IdentificationIcon class="h-6 w-6 mr-2"/></div><div>Mock Interviews</div></div></NuxtLink></div>
+            </div></nav>
           <div>
             <slot />
           </div>
 </template>
+<script setup lang="ts">
+import {ClipboardDocumentIcon,BuildingOfficeIcon,IdentificationIcon} from '@heroicons/vue/24/outline'
+</script>
+
